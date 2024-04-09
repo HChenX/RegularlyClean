43e3b225c8d04c06d6a6a47c981cae48eac09870
HChen <HChen_LXi@outlook.com>
2024-04-10 00:11:05 +0800

    修复BUG

--- customize.sh
+++ customize.sh
@@ -8,7 +8,7 @@ path_old="/data/media/0/Android/RegularlyClean_old"
 
 [[ -d $path ]] && {
   outPut "杀死正在运行的crond进程···"
-  crond_pid="$(pgrep -f 'regularly.d' | grep -v $$)"
+  crond_pid="$(pgrep -f 'RegularlyClean' | grep -v $$)"
   [[ -n "$crond_pid" ]] && {
     for kill_pid in $crond_pid; do
       kill -9 "$kill_pid" && outPut "杀死crond进程: $kill_pid"
